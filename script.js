a7f766c17e87216730071f2bb23f945671e9626f
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2024-09-26 17:23:03 +0300

    refactoring the code

--- script.js
+++ script.js
@@ -1,55 +1,77 @@
-console.log("file running.");
-
 const myBtn=document.getElementById("btn")
+const statusEl=document.getElementById('status')
+const locationEl=document.getElementById('location')
 
-        // Function to display location
-        function showLocation(position) {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            const accuracy = position.coords.accuracy;
+let latitude = ''
+let longitude = ''
 
-            document.getElementById('status').innerText = "Location Found!";
-            document.getElementById('location').innerHTML = `
-                Latitude: ${latitude} <br>
-                Longitude: ${longitude} <br>
-                Accuracy: ${accuracy} meters
-            `;
+let latitudesArray=[]
+let longitudesArray=[]
+
+//function to push latitudes and longitudes to arrays
+
+function pushCordinates() {
+    longitudesArray.push(longitude)
+    latitudesArray.push(latitude)
+}
+// Function to display location
+function showLocation(position) {
+    latitude = position.coords.latitude
+    longitude = position.coords.longitude
+    //const accuracy = position.coords.accuracy
+    console.log(latitude)
+    console.log(longitude)
+    
+    
+    renderCordinates()
+
         }
 
-        // Error handler
-        function showError(error) {
-            let message = '';
-            switch (error.code) {
-                case error.PERMISSION_DENIED:
-                    message = "User denied the request for Geolocation.";
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    message = "Location information is unavailable.";
-                    break;
-                case error.TIMEOUT:
-                    message = "The request to get user location timed out.";
-                    break;
-                case error.UNKNOWN_ERROR:
-                    message = "An unknown error occurred.";
-                    break;
-            }
-            document.getElementById('status').innerText = message;
-        }
+// Error handler
+function showError(error) {
+    let message = ''
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            message = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            message = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            message = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            message = "An unknown error occurred."
+            break;
+    }
+    statusEl.innerText = message
+}
 
-        // Request user's location
-        function runCode() {
-            if (navigator.geolocation) {
-                document.getElementById('status').innerText = "Requesting location...";
-                navigator.geolocation.getCurrentPosition(showLocation, showError, {
-                    enableHighAccuracy: true, // Request high accuracy
-                    timeout: 500, // Timeout if no response
-                    maximumAge: 0 // Disable cached data
-                });
-            } else {
-                document.getElementById('status').innerText = "Geolocation is not supported by this browser.";
-            }
-        }
+// Request user's location
+function runCode() {
+    if (navigator.geolocation) {
+        statusEl.innerText = "Requesting location..."
+        navigator.geolocation.getCurrentPosition(showLocation, showError, {
+            enableHighAccuracy: true, // Request high accuracy
+            timeout: 5000, // Timeout if no response
+            maximumAge: 0 // Disable cached data
+        })
+    } else {
+        showBrowserError()
+    }
+}
+//function to show error when browser does not support geolocation  API
+function showBrowserError() {
+    statusEl.innerText = "Geolocation is not supported by this browser."
+}
 myBtn.addEventListener("click",runCode)
 
-
+function renderCordinates() {
+    statusEl.innerText = "Location Found!"
+    locationEl.innerHTML = `
+        Latitude: ${latitude} <br>
+        Longitude: ${longitude} <br>
+        
+    `
+}
 
